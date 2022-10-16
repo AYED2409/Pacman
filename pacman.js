a8955c3069bc0075f4437421ec3407e98fec2dac
AYED2409 <ayed2409@gmail.com>
2022-10-16 17:36:07 +0200

    Pacman Finalizado

--- pacman.js
+++ pacman.js
@@ -1,137 +1,155 @@
 var pacman = pacman || {}
-
 pacman ={
     Pacman:class{
         constructor(){
+            //posiciones del mapa
             this.array =[
                 [0,'x',2,3,4],
                 [10,11,12,'x',14],
                 [20,'x',22,23,24],
                 [30,'x','x',33,'x'],
                 [40,41,42,43,44]
-            ]
-            this.posicionPacman=[]
-            this.posicionFantasma=[]
+            ];
+            //posicion Actual de Pacman y fantasma
+            this.posicionPacman=[];
+            this.posicionFantasma=[];
+        }
+        //inicio del juego
+        init(){
+            this.GenerarPosicionPacman();
+            this.GenerarPosicionFantasma();
+            this.ComprobarBotones();
         }
         PintarMapa(){
-
-            var div =document.getElementById('tabla')
-            div.innerHTML=""
+            var div =document.getElementById('tabla');
+            div.innerHTML="";
             for(let i=0;i<this.array.length;i++){
-                let tr=document.createElement('tr')
-                div.appendChild(tr)
-                //console.log("array n: "+i)
+                let tr=document.createElement('tr');
+                div.appendChild(tr);
                 for(let j=0;j<this.array[i].length;j++){
-                    let td=document.createElement('td')
-                    td.style.textAlign='center'
-                    td.style.width='150px'
-                    td.style.height='150px'
+                    let td=document.createElement('td');
+                    td.style.textAlign='center';
+                    td.style.width='150px';
+                    td.style.height='150px';
                     if(this.array[i][j]=='x'){
-                        td.style.backgroundColor='black'
+                        td.style.backgroundColor='black';
                     }
                     if(this.array[i][j]=='C'){
-                        td.style.backgroundColor='yellow'
-                        this.posicionPacman[0]=i
-                        this.posicionPacman[1]=j
+                        td.style.backgroundColor='yellow';
+                        this.posicionPacman[0]=i;
+                        this.posicionPacman[1]=j;
                     }
                     if(this.array[i][j]=='F'){
-                        td.style.backgroundColor='red'
-                        this.posicionFantasma[0]=i
-                        this.posicionFantasma[1]=j
+                        td.style.backgroundColor='red';
+                        this.posicionFantasma[0]=i;
+                        this.posicionFantasma[1]=j;
                     }
-                   // td.innerHTML=this.array[i][j]
                     tr.appendChild(td)
-                    //console.log("valor:"+this.array[i][j] )
                 }
             }
         }
         GenerarPosicionPacman(){
-            let random1=Math.floor(Math.random()*this.array.length)
-            let random2=Math.floor(Math.random()*this.array.length)
-            //console.log(random1+"este es el random1")
-            //console.log(random2+"este es el random2")
+            let random1=Math.floor(Math.random()*this.array.length);
+            let random2=Math.floor(Math.random()*this.array.length);
             if(this.array[random1][random2]=='x'){
-              //  console.log("no se puede esta posicion")
-                this.GenerarPosicionPacman()
+                this.GenerarPosicionPacman();
             }else{
-               // console.log("si se puede esta posicion")
-                this.array[random1][random2]='C'
+                this.array[random1][random2]='C';
             }
-            document.getElementById('tabla').innerHTML=""
-            this.PintarMapa()
+            document.getElementById('tabla').innerHTML="";
+            //this.PintarMapa()
         }
         GenerarPosicionFantasma(){
-            let random1=Math.floor(Math.random()*this.array.length)
-            let random2=Math.floor(Math.random()*this.array.length)
-            //console.log(random1+"este es el random1")
-            //console.log(random2+"este es el random2")
+            let random1=Math.floor(Math.random()*this.array.length);
+            let random2=Math.floor(Math.random()*this.array.length);
             if(this.array[random1][random2]=='x' || this.array[random1][random2]=='C'){
-              //  console.log("no se puede esta posicion")
-                this.GenerarPosicionFantasma()
+                this.GenerarPosicionFantasma();
             }else{
-                //console.log("si se puede esta posicion")
-                this.array[random1][random2]='F'
+                this.array[random1][random2]='F';
             }
-            document.getElementById('tabla').innerHTML=""
-            this.PintarMapa()
+            document.getElementById('tabla').innerHTML="";
+            this.PintarMapa();
         }
         ComprobarBotones(){
-            let botones=document.getElementById('botones')
-           // console.log(botones)
+            let botones=document.getElementById('botones');
             botones.onclick=(e) =>{
                 if(e.target.id=='arriba'){
-                    console.log("pulsaste arriba")
-                    this.Mover((this.posicionPacman[0])-1,this.posicionPacman[1])
-                    this.PintarMapa()
+                    this.Mover((this.posicionPacman[0])-1,this.posicionPacman[1],this.posicionPacman,this.posicionFantasma);
+                    this.MoverFantasma();
+                    this.PintarMapa();
                 }else if(e.target.id=='abajo'){
-                    console.log("pulsaste abajo")
-                    this.Mover((this.posicionPacman[0])+1,this.posicionPacman[1])
-                    this.PintarMapa()
+                    this.Mover((this.posicionPacman[0])+1,this.posicionPacman[1],this.posicionPacman,this.posicionFantasma);
+                    this.MoverFantasma();
+                    this.PintarMapa();
                 }else if(e.target.id=='derecha'){
-                    console.log("pulsaste derecha")
-                    this.Mover(this.posicionPacman[0],(this.posicionPacman[1])+1)
-                    this.PintarMapa()
+                    this.Mover(this.posicionPacman[0],(this.posicionPacman[1])+1,this.posicionPacman,this.posicionFantasma);
+                    this.MoverFantasma();
+                    this.PintarMapa();
                 }else if(e.target.id=='izquierda'){
-                    console.log("pulsaste izquierda")
-                    this.Mover(this.posicionPacman[0],(this.posicionPacman[1])-1)
-                    this.PintarMapa()
+                    this.Mover(this.posicionPacman[0],(this.posicionPacman[1])-1,this.posicionPacman,this.posicionFantasma);
+                    this.MoverFantasma();
+                    this.PintarMapa();
                 }
             }
         }
-        Mover(pos1,pos2){
-            console.log("posicion 1 ::::"+pos1)
-            console.log("posicion2 ::::"+pos2)
+        Mover(pos1,pos2,arrayPropio,arrayEnemigo){
             if(pos1>this.array.length-1 || pos2>this.array.length-1 || pos1<0 || pos2<0 || this.array[pos1][pos2]=='x'){
                 console.log("movimiento incorrecto")
             }else{
-                console.log("posicion actual"+this.posicionPacman[0]+" "+this.posicionPacman[1])
-                this.array[pos1][pos2]='C'
-                this.array[this.posicionPacman[0]][this.posicionPacman[1]]=''
-                this.posicionPacman[0]=pos1
-                this.posicionPacman[1]=pos2
-                if(this.posicionPacman[0]==this.posicionFantasma[0] && this.posicionPacman[1]==this.posicionFantasma[1]){
-                    //console.log("As perdido")
-                    document.getElementById('mensaje').innerHTML='Perdiste!!!!!'
-                    this.array[this.posicionFantasma[0]][this.posicionFantasma[1]]='F'
-                    this.PintarMapa()
-                    document.getElementById('botones').style.pointerEvents='none'
+                this.array[pos1][pos2]=this.array[arrayPropio[0]][arrayPropio[1]];
+                this.array[arrayPropio[0]][arrayPropio[1]]='';
+                arrayPropio[0]=pos1;
+                arrayPropio[1]=pos2;
+                if(arrayPropio[0]==arrayEnemigo[0] && arrayPropio[1]==arrayEnemigo[1]){
+                    document.getElementById('mensaje').innerHTML='Perdiste!!!!!';
+                    this.array[arrayEnemigo[0]][arrayEnemigo[1]]='F';
+                    this.PintarMapa();
+                    document.getElementById('botones').style.pointerEvents='none';
                 }
-                console.log("posicion a mover :"+pos1+" "+pos2)
-                console.log(this.array[pos1][pos2])
+            }
+        }
+        MoverFantasma(){
+            let random1=Math.floor(Math.random()*4);
+            if(random1==0){
+                console.log("arriba")
+                if(this.posicionFantasma[0]>0 && this.array[this.posicionFantasma[0]-1][this.posicionFantasma[1]]!='x'){
+                    //console.log("si puede subir")
+                    //console.log("posicionFantasma0---_>: "+this.posicionFantasma[0])
+                    this.Mover((this.posicionFantasma[0])-1,this.posicionFantasma[1],this.posicionFantasma,this.posicionPacman);
+                }else{
+                    //console.log("no puede subir")
+                    this.MoverFantasma()
+                }
+            }else if(random1==1){
+                console.log("izquierda")
+                if(this.posicionFantasma[1]>0 && this.array[this.posicionFantasma[0]][this.posicionFantasma[1]-1]!='x'){
+                    this.Mover((this.posicionFantasma[0]),this.posicionFantasma[1]-1,this.posicionFantasma,this.posicionPacman);
+                }else{
+                    //console.log("no puede izquierda")
+                    this.MoverFantasma();
+                }
+                
+            }else if(random1==2){
+                console.log("abajo")
+                if(this.posicionFantasma[0]<this.array.length-1 && this.array[this.posicionFantasma[0]+1][this.posicionFantasma[1]]!='x'){
+                    this.Mover((this.posicionFantasma[0])+1,this.posicionFantasma[1],this.posicionFantasma,this.posicionPacman);
+                }else{
+                    //console.log("no puede bajar")
+                    this.MoverFantasma()
+                }
+                
+            }else if(random1==3){
+                console.log("derecha")
+                if(this.posicionFantasma[1]<this.array.length-1 && this.array[this.posicionFantasma[0]][this.posicionFantasma[1]+1]!='x'){
+                    this.Mover((this.posicionFantasma[0]),this.posicionFantasma[1]+1,this.posicionFantasma,this.posicionPacman);
+                }else{
+                   // console.log("no puede derecha")
+                    this.MoverFantasma()
+                }
+                
             }
         }
     } 
 }
-
-
 var juego = new pacman.Pacman()
-//juego.PintarMapa()
-//console.log(juego)
-//var posArr=0
-//let v = juego.array[0].indexOf('C')
-//console.log(v)
-
-juego.GenerarPosicionPacman()
-juego.GenerarPosicionFantasma()
-juego.ComprobarBotones()
-//juego.Mover(0,1)
+juego.init()
